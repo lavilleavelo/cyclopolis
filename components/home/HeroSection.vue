48109ed3549b3b567411c1d0f02263c9c34a1060
Collectif Cycliste 37 <amenagement@cc37.org>
2024-10-31 07:08:24 +0000

    Mise à jour mineure.

--- components/home/HeroSection.vue
+++ components/home/HeroSection.vue
@@ -39,7 +39,7 @@
               <span
                 class="mt-1 block text-4xl tracking-tight font-extrabold sm:text-5xl xl:text-6xl"
               >
-                <span class="block text-gray-900">Avancement des</span>
+                <span class="block text-gray-900">Avancement du</span>
                 <span class="block text-lvv-blue-600">{{ getRevName() }}</span>
               </span>
             </h1>
