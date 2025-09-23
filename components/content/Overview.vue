83400de6fe0f55fbc7699a28c94d899ae86cdc9a
hugues.verlin <hugues.verlin@alteryx.com>
2025-09-23 16:40:20 +0200

    feat: Add a button to download trace as GPX

--- components/content/Overview.vue
+++ components/content/Overview.vue
@@ -15,6 +15,19 @@
     <section aria-labelledby="shipping-heading" class="mt-10">
       <ClientOnly>
         <Map :features="features" :options="mapOptions" style="height: 40vh" />
+        <div class="mt-2 flex justify-end gap-4">
+        <button
+          type="button"
+          title="Télécharger le tracé au format GPX"
+          class="text-base font-semibold text-gray-500 hover:text-lvv-blue-600"
+          @click="downloadGpx">GPX</button> 
+        <a 
+          :href="linkToGeoJSON"
+          target="_blank" 
+          title="Voir le fichier GEOJSON sur GitHub"
+          class="text-base font-semibold text-gray-500 hover:text-lvv-blue-600 no-underline"
+          rel="noopener noreferrer">GEOJSON</a>
+        </div>
       </ClientOnly>
     </section>
   </div>
@@ -22,6 +35,7 @@
 
 <script setup lang="ts">
 import type { Collections } from '@nuxt/content';
+import GeoJsonToGpx from "@dwayneparton/geojson-to-gpx";
 
 
 const { path } = useRoute();
@@ -49,4 +63,38 @@ const features: Ref<Collections['voiesCyclablesGeojson']['features']> = computed
 
 const color = getLineColor(Number(voie.line));
 const distance = geojson.value ? getTotalDistance([geojson.value]) : 0;
+
+const linkToGeoJSON = `https://github.com/lavilleavelo/cyclopolis/blob/main/content/voies-cyclables/ligne-${voie.line}.json`;
+
+function downloadGpx() {
+  if (!geojson.value) {
+    return;
+  }
+
+  const gpx = GeoJsonToGpx(geojson.value, {
+    creator: 'Cyclopolis - La Ville à Vélo',
+    metadata: {
+      name: `Voie Lyonnaise ${voie.line}`,
+      desc: `Tracé de la voie lyonnaise ${voie.line} - Source: La Ville à Vélo`,
+      author: {
+        name: 'Cyclopolis - La Ville à Vélo',
+        link: { 
+          href: `https://cyclopolis.fr/voie-lyonnaise-${voie.line}`,
+          text: 'Cyclopolis - La Ville à Vélo'
+        }
+      }
+    }
+  });
+
+  const gpxString = new XMLSerializer().serializeToString(gpx);
+  const dataStr = `data:application/gpx+xml;charset=utf-8,${encodeURIComponent(gpxString)}`;
+
+  const downloadAnchorNode = document.createElement('a');
+  downloadAnchorNode.setAttribute("href", dataStr);
+  downloadAnchorNode.setAttribute("download", `voie-lyonaise-${voie.line}.gpx`);
+  document.body.appendChild(downloadAnchorNode);
+  downloadAnchorNode.click();
+  downloadAnchorNode.remove();
+}
+
 </script>
