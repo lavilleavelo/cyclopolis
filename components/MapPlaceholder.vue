da1242ca269137ed93e35410290ffeff232ab0e7
hverlin <hugues.verlin@gmail.com>
2025-11-20 15:26:34 +0100

    feat: utilisation des URLSearchParams pour stocker l'état de la page d'évolution

---
+++ components/MapPlaceholder.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="flex-1 flex items-center justify-center bg-[#F8F4F0]">
+      <Icon name="svg-spinners:ring-resize" class="w-16 h-16 text-lvv-blue-600"/>
+    </div>
+</template>
